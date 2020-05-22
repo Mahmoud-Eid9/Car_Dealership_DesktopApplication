8b6a63246e98e706530648b6db12236917905dc3
mahmoud-eid9 <mahmoudeid947@gmail.com>
2020-05-23 00:04:49 +0200

    Update Main.java

--- Car-dealership/src/application/Main.java
+++ Car-dealership/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -17,7 +18,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			HBox root = new HBox(0);
-			VBox vbox = new VBox();
+			root.setMaxSize(1100, 650);
+			root.setMinSize(1100, 650);
+			root.setAlignment(Pos.CENTER_LEFT);
+			VBox vbox = new VBox(20);
+			vbox.setAlignment(Pos.CENTER);
+			vbox.setPadding(new Insets(20, 15, 5, 50));
 			Scene scene = new Scene(root,1100,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			TextField username = new TextField();
