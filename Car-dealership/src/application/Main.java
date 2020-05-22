6ef41ec6d17a778e1dbf958e4193c4d6634d55cc
mahmoud-eid9 <mahmoudeid947@gmail.com>
2020-05-22 23:53:13 +0200

    Update Main.java

--- Car-dealership/src/application/Main.java
+++ Car-dealership/src/application/Main.java
@@ -1,18 +1,31 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			HBox root = new HBox(0);
+			VBox vbox = new VBox();
+			Scene scene = new Scene(root,1100,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			TextField username = new TextField();
+			TextField password = new TextField();
+			Button logIn = new Button("Log-In");
+			Button signUp = new Button("Sign-Up");
+			vbox.getChildren().addAll(username, password, logIn, signUp);
+			root.getChildren().add(vbox);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
